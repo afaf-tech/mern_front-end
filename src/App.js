925784cdf01713e75f9b07077095d254709f7cf3
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-19 00:17:52 +0700

    creating form components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import  'assets/scss/style.scss';
 import LandingPage from 'elements/pages/LandingPage';
+import Example from 'elements/pages/Example';
 
 function App() {
   return (
     <div className="App">
         <Router>
-            <Route path="/" component={LandingPage}></Route>
+            <Route exact path="/" component={LandingPage}></Route>
+            <Route path="/example" component={Example}></Route>
         </Router>
     </div>
   );
