34c1fa0a0ebb8165296303e32ea02c1457ffee17
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-19 00:41:30 +0700

    breadcrumbs component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import  'assets/scss/style.scss';
 import LandingPage from 'elements/pages/LandingPage';
 import Example from 'elements/pages/Example';
+import Example2 from 'elements/pages/Example2';
 
 function App() {
   return (
     <div className="App">
         <Router>
             <Route exact path="/" component={LandingPage}></Route>
-            <Route path="/example" component={Example}></Route>
+            <Route exact path="/example" component={Example}></Route>
+            <Route path="/example2" component={Example2}></Route>
         </Router>
     </div>
   );
