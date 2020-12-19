386df61a137165e6395c3c142f3940f5c81c4023
fatihalfikri <fatihalfikri5@gmail.com>
2020-12-19 20:03:46 +0700

    improve header parts for checkout page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import  'assets/scss/style.scss';
 import LandingPage from 'elements/pages/LandingPage';
+import Checkout from 'elements/pages/Checkout';
 import DetailsPage from 'elements/pages/DetailsPage';
 import Example from 'elements/pages/Example';
 import Example2 from 'elements/pages/Example2';
@@ -12,6 +13,7 @@ function App() {
         <Router>
             <Route exact path="/" component={LandingPage}></Route>
             <Route exact path="/properties/:id" component={DetailsPage}></Route>
+            <Route exact path="/checkout" component={Checkout}></Route>
             <Route exact path="/example" component={Example}></Route>
             <Route path="/example2" component={Example2}></Route>
         </Router>
---
+++ src/elements/pages/Checkout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from 'elements/parts/Header'
+export default function Checkout() {
+    return (
+        <>
+            <Header isCentered />
+        </>
+    )
+}
